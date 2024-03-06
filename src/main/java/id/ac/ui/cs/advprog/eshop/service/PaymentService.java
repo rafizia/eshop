8a27685055e1d93c9ba307a497d4cd76a2e82dee
Muhammad Rafi Zia Ulhaq <mohrafiziaulhaq@gmail.com>
2024-03-06 18:48:30 +0700

    [RED] Add PaymentServiceImpl skeleton

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
@@ -2,11 +2,12 @@ package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.model.Order;
+import java.util.List;
 import java.util.Map;
 
 public interface PaymentService {
     Payment addPayment(Order order, String method, Map<String, String> paymentData);
     Payment setStatus(Payment payment, String status);
     Payment getPayment(String paymentId);
-    Payment getAllPayments();
+    List<Payment> getAllPayments();
 }
